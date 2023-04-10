93b0ed80811daa3b5ab2a51421b724c7f5b03279
rohit-rautt <rohit.raut@edcast.com>
2023-04-10 16:06:35 +0530

    Added first app

---
+++ react-microfrontned-1/webpack.config.js
@@ -0,0 +1,45 @@
+/**
+ * ******************* MFE 1 *******************
+ */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+  },
+  module: {
+    rules: [
+      {
+        /* The following line to ask babel 
+        to compile any file with extension
+        .js */
+        test: /\.js?$/,
+
+        /* exclude node_modules directory from babel. 
+        Babel will not compile any files in this directory*/
+        exclude: /node_modules/,
+
+        // To Use babel Loader
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env" /* to transfer any advansed ES to ES5 */,
+            "@babel/preset-react",
+          ], // to compile react to ES5
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "reactMFE1",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MFIndex": "./src/index",
+      },
+    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
+};
